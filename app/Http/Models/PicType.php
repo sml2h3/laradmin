bd9ff9eda5d9f01495ea0ebfc6639c59658d4a65
sml2h3 <sml2h3@gmail.com>
2017-03-13 20:34:48 +0800

    后端菜单composer化、添加房源页面

---
+++ app/Http/Models/PicType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PicType extends Model
+{
+    protected $table = "pic_type";
+    protected $primaryKey = "Id";
+    public $timestamps = false;
+}
